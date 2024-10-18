ca6d41251bfbba5b6f3659c97e9b8dd669d40c54
Alex3253 <152949338+Alex3253@users.noreply.github.com>
2024-10-17 19:23:37 -0500

    Delete js directory

--- js/CL4_02-09-24/JavaScript/app01.js
+++
@@ -1,1 +0,0 @@
-alert('Curso de html...')
--- js/CL4_02-09-24/JavaScript/app02.js
+++
@@ -1,8 +0,0 @@
-//Mostrar info mediante una ventana emergente
-alert('Instituto CERTUS');
-
-//Mostrar informacion en el documento de html
-document.write('Curso de Backend');
-
-//Mostrar info en la consola
-console.log('Aplicacion de NODE JS');
